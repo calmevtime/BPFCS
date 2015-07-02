4bc7977fb647fde1c8be6ce47444925d2f3e6dc9
calmevtime <calmevtime@gmail.com>
2015-07-01 22:03:34 -0700

    modify testFramework

--- testFramework.m
+++ testFramework.m
@@ -4,17 +4,19 @@ clc
 poolobj = gcp('nocreate'); % If no pool, do not create new one.
 if isempty(poolobj)
     poolsize = 0;
-    parpool('local',4);
+    parpool('local',20);
 else
     poolsize = poolobj.NumWorkers;
 end
 
-sweep_Lambda_samples_withoutPre;
-sweep_Lambda_samples_PreDCT;
-sweep_Lambda_samples_PreDWT;
-sweep_K_samples_withoutPre;
+% sweep_Lambda_samples_withoutPre;
+
+% sweep_Lambda_samples_PreDCT;
+% sweep_Lambda_samples_PreDWT;
+% sweep_K_samples_withoutPre;
 
 demoSweepWithoutPre;
+demoSweepFilter;
 % demoSweepDctPre;
 % demoSweepDwtPre;
 
